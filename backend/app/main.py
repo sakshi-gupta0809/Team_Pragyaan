b4927066097cd9c3ba330bfbb54ba19ca728303d
sakshi-gupta-0809 <sakshi.gupta@neutrinotechlabs.com>
2025-09-11 18:19:38 +0530

    error resolve

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ logger.info("Starting Scalable Email Platform API")
 # -------------------- Middleware --------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5174"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
